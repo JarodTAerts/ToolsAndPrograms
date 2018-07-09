330794042ba72fff09102f6a5f99a5fec4806e86
Jarod Aerts <jarod2017@gmail.com>
2018-07-09 14:09:27 -0500

    Created the HTML handler class, but did not do any work in it

---
+++ src/OfflineWikipedia/OfflineWikipedia/OfflineWikipedia/Helpers/HTMLHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfflineWikipedia.Helpers
+{
+    /// <summary>
+    /// Class to handle the parsing and processing of HTML Text
+    /// </summary>
+    class HTMLHandler
+    {
+
+    }
+}
